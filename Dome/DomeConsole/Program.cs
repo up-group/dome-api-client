0309cb7e8fdcfb198cdca8d42fa58e1209153b81
SunTzu <171946@supinfo.com>
2017-01-17 14:23:09 +0100

    init

---
+++ Dome/DomeConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using Dome.Authentification;
+using System.ServiceModel;
+
+namespace DomeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress(@"http://159.122.2.36/oauth-soap/DOME_BUS_EAI_R001loginWS");
+            var client = new DOME_BUS_EAI_R001loginWSClient(binding, endpoint);
+
+            var result = client.login(new userInfoDto { username = "op.lab4", password = "5747a0021eb349e9c8d3667cf1a5e9ec" });
+        }
+    }
+}
