e27fd89fb67323d16232d1cc81bb24aabaf06557
SunTzu <171946@supinfo.com>
2017-01-18 15:01:17 +0100

    ajout test

---
+++ Dome/Dome/AuthentificationHelper.cs
@@ -0,0 +1,61 @@
+﻿using Dome.Authentification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dome
+{
+    public static class AuthentificationHelper
+    {
+
+        private static authenticationDto _auth;
+        public static authenticationDto auth
+        {
+            get
+            {
+                if (_auth == null)
+                {
+                    init();
+                }
+                return _auth;
+            }
+        }
+
+
+
+        private static void init()
+        {
+
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress(ConstHelper.urlbase + "/oauth-soap/DOME_BUS_EAI_R001loginWS");
+            var client = new DOME_BUS_EAI_R001loginWSClient(binding, endpoint);
+            _auth = client.login(new userInfoDto { username = ConstHelper.username, password = CreateMD5(ConstHelper.password).ToLower() });
+
+        }
+
+
+        private static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+
+
+
+}
---
+++ Dome/Dome/DomeCallHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dome
+{
+    public class DomeCallHelper
+    {
+        //public static T1 call<T1, T2>() where T2 : class where T1 : ClientBase<T2>
+        //{
+        //    var url = ConstHelper.urlbase + "/soap/" + typeof(T2).Name;
+        //    var binding = new BasicHttpBinding();
+        //    var endpoint = new EndpointAddress(url);
+        //    var client = (T1)Activator.CreateInstance(typeof(T1), new object[] { binding, endpoint });
+
+        //    //using (new OperationContextScope(client.InnerChannel))
+        //    //{
+        //    new OperationContextScope(client.InnerChannel);
+
+        //    HttpRequestMessageProperty request = new HttpRequestMessageProperty();
+        //    request.Headers["Authorization"] = "Bearer " + AuthentificationHelper.auth.token;
+
+        //    OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = request;
+
+
+        //    return client;
+
+        //}
+
+        public static T3 call2<T1, T2,T3>(Func<T1, T3> test) where T2 : class where T1 : ClientBase<T2>
+        {
+            var url = ConstHelper.urlbase + "/soap/" + typeof(T2).Name;
+            var binding = new BasicHttpBinding();
+            var endpoint = new EndpointAddress(url);
+            var client = (T1)Activator.CreateInstance(typeof(T1), new object[] { binding, endpoint });
+
+            using (new OperationContextScope(client.InnerChannel))
+            {
+
+                HttpRequestMessageProperty request = new HttpRequestMessageProperty();
+                request.Headers["Authorization"] = "Bearer " + AuthentificationHelper.auth.token;
+
+                OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = request;
+                var data = test(client);
+                return data;
+
+            }
+        }
+    }
+
+}
