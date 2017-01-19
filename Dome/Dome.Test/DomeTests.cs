5fc9f777e032b0db9b2838dea7c5e9c9ab043a9c
Nyx0uf <bgd@ARCAN-SYSTEMS.LAN>
2017-01-19 11:08:10 +0100

    modif tests

--- Dome/Dome.Test/DomeTests.cs
+++ Dome/Dome.Test/DomeTests.cs
@@ -55,17 +55,17 @@ namespace Dome.Test
                 personBirthName = "",
                 personCedex = "",
                 personCityName = "Lyon",
-                personCityZipCode = "69003",
+                personCityZipCode = "69001",
                 personCivilityId = (int)civilite.Monsieur,
                 personCivilityIdSpecified = true,
                 personComment = "personne test",
                 personEmail1 = "test@test.fr",
                 personEmail2 = "",
-                personFirstName = "guillaume",
+                personFirstName = "person",
                 personINSC = "",
                 personINSA = "",
                 personJob = "",
-                personLastName = "dumont",
+                personLastName = "person",
                 personLieuDit = "",
                 personMobilePhoneNumber = "",
                 personNIR = "",
@@ -98,17 +98,17 @@ namespace Dome.Test
                 personBirthName = "",
                 personCedex = "",
                 personCityName = "Lyon",
-                personCityZipCode = "69003",
+                personCityZipCode = "69002",
                 personCivilityId = (int)civilite.Monsieur,
                 personCivilityIdSpecified = true,
-                personComment = "personne test",
+                personComment = "patient test",
                 personEmail1 = "test@test.fr",
                 personEmail2 = "",
-                personFirstName = "guillaume",
+                personFirstName = "patient",
                 personINSC = "",
                 personINSA = "",
                 personJob = "",
-                personLastName = "dumont",
+                personLastName = "patient",
                 personLieuDit = "",
                 personMobilePhoneNumber = "",
                 personNIR = "",
@@ -127,7 +127,62 @@ namespace Dome.Test
             {
                 accountId = resp.accountId,
                 accountIdSpecified = true,
-                profileCibleType = 10,
+                profileCibleType = (int)Profile.Beneficiaire,
+                profileCibleTypeSpecified = true
+            };
+
+            /*var res = DomeCall.createProfile(profile);
+
+            Assert.IsTrue(res.statusId == 0);*/
+        }
+
+        [TestMethod]
+        public void CreationEntourageShouldSucceed()
+        {
+            AuthentificationHelper.Instance.connect(settings.username, settings.password);
+
+            Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
+            Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
+
+            var person = new CreatePersonInnerDto()
+            {
+                personAddressComp1 = "63 avenue lacassagne",
+                personAddressComp2 = "",
+                personBirthDate = new DateTime(1993, 9, 16),
+                personBirthDateSpecified = true,
+                personBirthName = "",
+                personCedex = "",
+                personCityName = "Lyon",
+                personCityZipCode = "69003",
+                personCivilityId = (int)civilite.Monsieur,
+                personCivilityIdSpecified = true,
+                personComment = "proche test",
+                personEmail1 = "test@test.fr",
+                personEmail2 = "",
+                personFirstName = "proche",
+                personINSC = "",
+                personINSA = "",
+                personJob = "",
+                personLastName = "proche",
+                personLieuDit = "",
+                personMobilePhoneNumber = "",
+                personNIR = "",
+                personPhoneNumber = "",
+                personPostBox = "",
+                personRoadName = "rue lalala",
+                personRoadType = "",
+                personRoadNumber = "",
+                personRPPS = ""
+            };
+
+            CreatePersonResponseDto resp = DomeCall.createPerson(person);
+            Assert.IsTrue(resp.statusId == 0);
+
+            createProfileDto profile = new createProfileDto()
+            {
+                accountId = resp.accountId,
+                accountIdSpecified = true,
+                profileCibleType = (int)Profile.Proche,
                 profileCibleTypeSpecified = true
             };
 
--- Dome/Dome/Entourage.cs
+++ Dome/Dome/Entourage.cs
@@ -416,6 +416,22 @@ namespace Dome
         Madame = 2
     }
 
+    public enum Profile
+    {
+        Beneficiaire = 10,
+        Referent = 11,
+        Proche = 12,
+        StructureAidePersonne = 20,
+        OperateurStructure = 21,
+        PersonnelNonMedical = 22,
+        PersonnelParaMedical = 23,
+        PersonnelMedical = 24,
+        StructureMedicale = 30,
+        Urgentiste = 31,
+        OperateurStructureUrgence = 32,
+   
+    }
+
     public class MCP02
     {
 
