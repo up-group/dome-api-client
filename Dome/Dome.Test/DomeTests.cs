4fcc22aa74eafaec51601c25e270b55dddf113f5
Nyx0uf <bgd@ARCAN-SYSTEMS.LAN>
2017-01-19 15:03:12 +0100

    update test

--- Dome/Dome.Test/DomeTests.cs
+++ Dome/Dome.Test/DomeTests.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
 using Dome.R511;
 using Dome.R521;
+using Dome.R830a;
+using Dome.R542a;
 
 namespace Dome.Test
 {
@@ -17,6 +19,7 @@ namespace Dome.Test
             settings.username = ConfigurationManager.AppSettings["username"];
         }
 
+        #region Auth
         [TestMethod]
         public void AuthentificationShouldSucceed()
         {
@@ -37,7 +40,9 @@ namespace Dome.Test
             Assert.IsFalse(AuthentificationHelper.Instance.isConnected);
             Assert.IsTrue(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
         }
+        #endregion
 
+        #region Person
         [TestMethod]
         public void CreationPersonShouldSucceed()
         {
@@ -46,7 +51,7 @@ namespace Dome.Test
             Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
             Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
 
-            CreatePersonInnerDto person = new CreatePersonInnerDto()
+            R511.CreatePersonInnerDto person = new R511.CreatePersonInnerDto()
             {
                 personBirthDate = new DateTime(1988, 2, 8),
                 personBirthDateSpecified = true,
@@ -68,6 +73,41 @@ namespace Dome.Test
         }
 
         [TestMethod]
+        public void UpdatePersonShouldSucceed()
+        {
+            AuthentificationHelper.Instance.connect(settings.username, settings.password);
+
+            Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
+            Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
+
+            UpdatePersonDto person = new UpdatePersonDto()
+            {
+                accountId = 15,
+                accountIdSpecified = true,
+                DOME_createPerson = new R542a.CreatePersonInnerDto()
+                {
+                    personBirthDate = new DateTime(1988, 2, 8),
+                    personBirthDateSpecified = true,
+                    personBirthName = "person",
+                    personCityName = "PARIS",
+                    personCityZipCode = "75000",
+                    personCivilityId = (int)civilite.Madame,
+                    personCivilityIdSpecified = true,
+                    personFirstName = "person",
+                    personLastName = "person",
+                    personMobilePhoneNumber = "0601020304",
+                    personPhoneNumber = "0401020304",
+                    personRoadName = "rue lalala",
+                    personRoadNumber = "1337",
+                }
+            };
+
+            UpdatePersonResponseDto resp = DomeCall.updatePerson(person);
+            Assert.IsTrue(resp.statusId == 0);
+        }
+        #endregion
+
+        [TestMethod]
         public void CreationPatientShouldSucceed()
         {
             AuthentificationHelper.Instance.connect(settings.username, settings.password);
@@ -75,7 +115,7 @@ namespace Dome.Test
             Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
             Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
 
-            CreatePersonInnerDto person = new CreatePersonInnerDto()
+            R511.CreatePersonInnerDto person = new R511.CreatePersonInnerDto()
             {
                 personAddressComp1 = "63 avenue lacassagne",
                 personAddressComp2 = "",
@@ -130,7 +170,7 @@ namespace Dome.Test
             Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
             Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
 
-            CreatePersonInnerDto person = new CreatePersonInnerDto()
+            R511.CreatePersonInnerDto person = new R511.CreatePersonInnerDto()
             {
                 personAddressComp1 = "63 avenue lacassagne",
                 personAddressComp2 = "",
@@ -176,5 +216,162 @@ namespace Dome.Test
 
             Assert.IsTrue(res.statusId == 0);
         }
+
+        [TestMethod]
+        public void CreationSalarieShouldSucceed()
+        {
+            AuthentificationHelper.Instance.connect(settings.username, settings.password);
+
+            Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
+            Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
+
+            R511.CreatePersonInnerDto person = new R511.CreatePersonInnerDto()
+            {
+                personAddressComp1 = "63 avenue lacassagne",
+                personAddressComp2 = "",
+                personBirthDate = new DateTime(1993, 9, 16),
+                personBirthDateSpecified = true,
+                personBirthName = "",
+                personCedex = "",
+                personCityName = "Lyon",
+                personCityZipCode = "69004",
+                personCivilityId = (int)civilite.Monsieur,
+                personCivilityIdSpecified = true,
+                personComment = "salarie test",
+                personEmail1 = "test@test.fr",
+                personEmail2 = "",
+                personFirstName = "salarie",
+                personINSC = "",
+                personINSA = "",
+                personJob = "",
+                personLastName = "salarie",
+                personLieuDit = "",
+                personMobilePhoneNumber = "",
+                personNIR = "",
+                personPhoneNumber = "",
+                personPostBox = "",
+                personRoadName = "rue lalala",
+                personRoadType = "",
+                personRoadNumber = "",
+                personRPPS = ""
+            };
+
+            CreatePersonResponseDto resp = DomeCall.createPerson(person);
+            Assert.IsTrue(resp.statusId == 0);
+
+            createProfileDto profile = new createProfileDto()
+            {
+                accountId = resp.accountId,
+                accountIdSpecified = true,
+                profileCibleType = (int)Profile.PersonnelMedical,
+                profileCibleTypeSpecified = true
+            };
+
+            var res = DomeCall.createProfile(profile);
+
+            Assert.IsTrue(res.statusId == 0);
+        }
+
+        [TestMethod]
+        public void CreationStructureShouldSucceed()
+        {
+            AuthentificationHelper.Instance.connect(settings.username, settings.password);
+
+            Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
+            Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
+
+            R511.CreatePersonInnerDto person = new R511.CreatePersonInnerDto()
+            {
+                personAddressComp1 = "63 avenue lacassagne",
+                personAddressComp2 = "",
+                personBirthDate = new DateTime(1993, 9, 16),
+                personBirthDateSpecified = true,
+                personBirthName = "",
+                personCedex = "",
+                personCityName = "Lyon",
+                personCityZipCode = "69005",
+                personCivilityId = (int)civilite.Monsieur,
+                personCivilityIdSpecified = true,
+                personComment = "structure test",
+                personEmail1 = "test@test.fr",
+                personEmail2 = "",
+                personFirstName = "structure",
+                personINSC = "",
+                personINSA = "",
+                personJob = "",
+                personLastName = "structure",
+                personLieuDit = "",
+                personMobilePhoneNumber = "",
+                personNIR = "",
+                personPhoneNumber = "",
+                personPostBox = "",
+                personRoadName = "rue lalala",
+                personRoadType = "",
+                personRoadNumber = "",
+                personRPPS = ""
+            };
+
+            CreatePersonResponseDto resp = DomeCall.createPerson(person);
+            Assert.IsTrue(resp.statusId == 0);
+
+            createProfileDto profile = new createProfileDto()
+            {
+                accountId = resp.accountId,
+                accountIdSpecified = true,
+                profileCibleType = (int)Profile.StructureMedicale,
+                profileCibleTypeSpecified = true
+            };
+
+            var res = DomeCall.createProfile(profile);
+
+            Assert.IsTrue(res.statusId == 0);
+        }
+
+        #region AGGIR
+        [TestMethod]
+        public void CreationGrilleAGGIRShouldSucceed()
+        {
+            AuthentificationHelper.Instance.connect(settings.username, settings.password);
+
+            Assert.IsTrue(AuthentificationHelper.Instance.isConnected);
+            Assert.IsFalse(String.IsNullOrWhiteSpace(AuthentificationHelper.Instance.auth.token));
+
+            addNewAGGIRDto aggir = new addNewAGGIRDto()
+            {
+                benefProfileIdSpecified = true,
+                benefProfileId = 15,
+                DOME_medAGGIR = new addNewAggirInnerDto()
+                {
+                    AGGIREvaluationDate = DateTime.Now.Date,
+                    AGGIRCreationProfileId = 40,
+                    AGGIRCreatorName = "moi",
+                    AGGIRCreationDate = DateTime.Now.Date,
+                    AGGIRCreatorEntityName = "Structure",
+                    AGGIRCode = 1,
+                    AGGIRCoherence = 1,
+                    AGGIROrientation = 1,
+                    AGGIRToilette = 1,
+                    AGGIRHabillage = 1,
+                    AGGIRAlimentation = 1,
+                    AGGIRElimination = 1,
+                    AGGIRTransferts = 1,
+                    AGGIRDeplacInt = 1,
+                    AGGIRDeplacExt = 1,
+                    AGGIRAlerter = 1,
+                    AGGIRGestion = 1,
+                    AGGIRCuisine = 1,
+                    AGGIRMenage = 1,
+                    AGGIRTransport = 1,
+                    AGGIRSuiviTraitement = 1,
+                    AGGIRTempsLibre = 1,
+                    AGGIRAchats = 1,
+                }
+            };
+
+            addNewAGGIRResponseDto resp = DomeCall.addNewAGGIR(aggir);
+
+            Assert.IsTrue(resp.statusId == 0);
+        }
+        #endregion
     }
 }
