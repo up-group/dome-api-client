ca9cd0b88302b5a800ad9654acba81604ecb14e0
SunTzu <171946@supinfo.com>
2017-02-28 14:12:35 +0100

    Ajout de test helper  !!! sans login mdp

---
+++ Dome/Dome.Test/TestHelper.cs
@@ -0,0 +1,18 @@
+﻿using Dome.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dome.Test
+{
+    public  class TestHelper
+    {
+        public static DomeClient GetNewClient()
+        {
+            return new DomeClient("hibault.marignier", "3802", "http://dev.mondome.fr/");
+        }
+
+    }
+}
